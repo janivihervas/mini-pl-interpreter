6b4c6456b21c40ae1db772aa14b2485463b04a35
Jani Viherväs <jani.p.vihervas@student.jyu.fi>
2014-02-28 09:23:25 +0200

    IO exception

---
+++ trunk/MiniPL/MiniPL.Exceptions/IOException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MiniPL.Exceptions
+{
+    /// @author Jani Viherväs
+    /// @version 28.2.2014
+    ///
+    /// <summary>
+    /// Class for IO exceptions
+    /// </summary>
+    public class IOException : Exception
+    {
+        public IOException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
